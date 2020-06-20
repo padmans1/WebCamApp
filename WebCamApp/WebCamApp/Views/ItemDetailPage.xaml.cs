b58399613bb651ff9a49774c91b9d9736f320a5e
Sriram Padmanbhan <ram.heman87@gmail.com>
2020-06-20 19:02:45 +0530

    Added code to the repository

---
+++ WebCamApp/WebCamApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using WebCamApp.Models;
+using WebCamApp.ViewModels;
+using Plugin.Media;
+
+namespace WebCamApp.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        //public ItemDetailPage()
+        //{
+        //    InitializeComponent();
+
+        //    var item = new Item
+        //    {
+        //        Text = "Item 1",
+        //        Description = "This is an item description."
+        //    };
+
+        //    viewModel = new ItemDetailViewModel(item);
+        //    BindingContext = viewModel;
+        //}
+
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            takePhoto.Clicked += async (sender, args) =>
+            {
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("File Location", file.Path, "OK");
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            };
+
+            pickPhoto.Clicked += async (sender, args) =>
+            {
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                    return;
+                }
+                var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+
+
+                if (file == null)
+                    return;
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            };
+
+            takeVideo.Clicked += async (sender, args) =>
+            {
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakeVideoSupported)
+                {
+                    await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakeVideoAsync(new Plugin.Media.Abstractions.StoreVideoOptions
+                {
+                    Name = "video.mp4",
+                    Directory = "DefaultVideos",
+                });
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("Video Recorded", "Location: " + file.Path, "OK");
+
+                file.Dispose();
+            };
+
+            pickVideo.Clicked += async (sender, args) =>
+            {
+                if (!CrossMedia.Current.IsPickVideoSupported)
+                {
+                    await DisplayAlert("Videos Not Supported", ":( Permission not granted to videos.", "OK");
+                    return;
+                }
+                var file = await CrossMedia.Current.PickVideoAsync();
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("Video Selected", "Location: " + file.Path, "OK");
+                file.Dispose();
+            };
+        }
+
+    }
+
+
+    
+}
